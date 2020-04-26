678d47c2ca2583a63784f9cae67eb972ac18eb4f
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-26 18:22:12 +0000

    Automatically backed up by Learn

--- spec/03_challenge_spec.rb
+++ spec/03_challenge_spec.rb
@@ -11,4 +11,12 @@ describe "#third_challenge" do
     expect(third_challenge[:montague][:hero].values).to eq(["Romeo", "15", "alive"])
   end
 
-  
+  it "fills out the Capulet character role keys with the key/value pairs describing their attributes" do 
+    expect(third_challenge[:capulet][:patriarch].keys).to eq([:name, :age])
+    expect(third_challenge[:capulet][:matriarch].keys).to eq([:name, :age])
+    expect(third_challenge[:capulet][:heroine].keys).to eq([:name, :age, :status])
+    expect(third_challenge[:capulet][:patriarch].values).to eq(["Lord Capulet", "50"])
+    expect(third_challenge[:capulet][:matriarch].values).to eq(["Lady Capulet", "51"])
+    expect(third_challenge[:capulet][:heroine].values).to eq(["Juliet", "15", "alive"])
+  end
+end
